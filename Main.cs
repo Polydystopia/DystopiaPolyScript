ade898c74e4877b2dded52c965a61d7edfc5ac28
Demonofpower <emil.peter21@gmail.com>
2025-06-07 20:57:24 +0200

    Added backend server path patch

--- Main.cs
+++ Main.cs
@@ -6,6 +6,7 @@ public static class Main
 {
     public static void Load(ManualLogSource logger)
     {
-        logger.LogMessage("Here we go!");
+        BuildConfigHelper.GetSelectedBuildConfig().buildServerURL = BuildServerURL.Custom;
+        BuildConfigHelper.GetSelectedBuildConfig().customServerURL = "http://localhost:5051";
     }
 }
